a551457a61c70d67b4653a2afb0a1bf5a5f11dc4
NMGVox <nijeemgreene@gmail.com>
2023-02-22 13:25:42 -0500

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function multiply(a,b){
 function divide(a,b) {
     return a/b;
 }
+
+function operate(str){
+    let operands = str.split(" ");
+    switch (operands[1]){
+        case "+":
+            return add(+operands[0], +operands[2]);
+            break;
+        case "-":
+            return subtract(+operands[0], +operands[2]);
+            break;
+        case "/":
+            return divide(+operands[0], +operands[2]);
+            break;
+        case "*":
+            return multiply(+operands[0], +operands[2]);
+            break;
+        default:
+            return("Something went terribly wrong here.");
+    }
+}
